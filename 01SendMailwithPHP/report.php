1c4514cdd76dd03aa06b96c3c369f3ab59b94249
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-09-24 08:14:52 -0600

    Update report.php

--- 01SendMailwithPHP/report.php
+++ 01SendMailwithPHP/report.php
@@ -1,12 +1,20 @@
-<?php
-                    $when_it_happened = $_POST['whenithappened'];
-                    $seen_dog = $_POST['seendog'];
-                    $comments = $_POST['comments'];
-                    $email = $_POST['email'];
-                
-                    echo 'Thanks for submitting the form.<br/>';
-                    echo 'You were abducted ' . $when_it_happened;
-                    echo 'Was Fang there? ' . $seen_dog . '>br/>';
-                    echo 'Your email address is ' . $email;
-                    mail($email,"Abduction Report",$comments);
-                ?>
+     <?php
+	      $name = $_POST['firstName'].' '.$_POST['lastName'];
+        $when_it_happened = $_POST['whenithappened'];
+        $seen_dog = $_POST['seendog'];
+        $comments = $_POST['comments'];
+        $email = $_POST['email'];
+        $to = 'ammon.packer@gmail.com';
+        $subject = "ALIENS ABDUCTED ME";
+        $msg = "$name was abducted $when_it_happened.\n" .
+		    "Fang spotted? $seen_dog. \n" .
+                "email is: $email\n" .
+              	"Other comments: $comments";              
+	      mail($to, $subject, $msg, 'From: ' . $email);
+       	echo 'Thanks for submitting the form.<br/>';
+        echo 'You were abducted on ' . $when_it_happened . '<br/>';
+	      if(isset($_POST['seendog'])){
+	      echo "Was Fang there? ".$_POST['seendog'] . '<br/>';  //  Displaying Selected Value
+	      }
+        echo 'Your email address is ' . $email;                
+     ?>
