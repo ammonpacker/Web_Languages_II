f4ace829bc510140b50e04ea9a11379501ed4dde
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-10-22 16:19:21 -0600

    Update and rename 03SpammingEmail0 to registration.php

---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+	$first_name = $_POST['firstName'];
+	$last_name = $_POST['lastName'];
+	$name = $_POST['firstName'].' '.$_POST['lastName'];
+	$email = $_POST['email'];
+	 
+	
+	//building database connection      	
+	$dbConnection = mysqli_connect('localhost','ammonp93_dgmusr','CA}x]ee1&G*%','ammonp93_dgm3760') or die('Connection to the Database failed.');           
+	
+	//build query
+	$query = "INSERT INTO Newsletter (first, last, email)" 	 
+	  ."VALUES('$first_name','$last_name','$email')";
+	
+	//talk to database
+	$result = mysqli_query($dbConnection, $query) or die("Query: ". $query."<p>Error: " .mysqli_error($dbConnection));
+	
+	//close connection
+	mysqli_close($dbConnection);
+	echo 'Thanks for signing up!.<br/>';               
+?>
