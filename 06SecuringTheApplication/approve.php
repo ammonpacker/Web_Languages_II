23bdad3704e49eea97336cdd45fae969baffbd80
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-11-29 00:34:31 -0700

    Create approve.php
    page that populates with posts that need approval

---
+++ 06SecuringTheApplication/approve.php
@@ -0,0 +1,69 @@
+<?php
+require_once('authorize.php');
+
+require_once('variable.php');
+
+//building database connection      	
+$dbc = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die('Connection to the Database failed.');           
+	
+//build query
+$query = "SELECT * FROM blog WHERE approved=0 ORDER BY date ASC";
+	
+//talk to database
+$result = mysqli_query($dbc, $query) or die("Query: ". $query."<p>Error: " .mysqli_error($dbc));
+				
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="Profile3/profile3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <title>Securing The Application</title>
+</head>
+
+<body>
+<?php include_once('header.php');?>
+<div id="content" class="container-fluid">
+     <div class="jumbotron jumbotron-fluid text-center">
+        <h1>Unapproved Posts</h1>
+     </div>
+     <?php
+     if(mysqli_num_rows($result) == 0){
+     	echo '<p> All posts have been approved. </p>';
+     }else {
+     	while ($row = mysqli_fetch_array($result)) {
+     		echo '<div class="card">';
+     		echo '<div class="card-body">';
+     		echo '<h3>'.$row['name'].'</h3>';     	
+     		echo '<b class="card-title">'.$row['topic'].'</b>';
+     		echo '<p class="card-text">'.$row['comment'].'</p>';
+     		echo '<p class="date">'.$row['date'].'</p>';
+     		echo '<a class="btn btn-success" href=approve2.php?id='.$row['id'].'>Approve</a>'.' ';
+     		echo '<a class="btn btn-danger" href=delete.php?id='.$row['id'].'>Delete</a>';
+     		echo '</div>';
+     		echo '</div>';
+     	}//end of while
+     }
+     ?>
+
+</div>
+
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
+
+
