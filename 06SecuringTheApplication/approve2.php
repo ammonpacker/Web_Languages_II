95fe157e5e36e5d3f08dfb717c675502b1590442
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-11-29 00:35:23 -0700

    Create approve2.php
    updates DB to approve posts for the blog

---
+++ 06SecuringTheApplication/approve2.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('authorize.php');
+
+$id = $_GET['id'];
+
+require_once('variable.php');
+
+//building database connection      	
+$dbc = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die('Connection to the Database failed.');           
+	
+//build query
+$query = "UPDATE blog SET approved=1 WHERE id=$id";
+	
+//talk to database
+$result = mysqli_query($dbc, $query) or die("Query: ". $query."<p>Error: " .mysqli_error($dbc));
+				
+//return to approve page
+header('Location: approve.php');
+?>
