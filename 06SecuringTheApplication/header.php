a3a090b0309e28be525ba0dab44760f645b9a317
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-11-29 00:38:49 -0700

    Create header.php
    creates the navbar for each page.

---
+++ 06SecuringTheApplication/header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="http://ammonp.sgedu.site">AP</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Blog Posts</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="comment.php">Make a Post</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="approve.php">Approve Posts</a>
+    </li>
+  </ul>
+</nav>
+<br>
