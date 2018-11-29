bca065a75fb0d928b044f4883c762575ffc3a433
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-11-29 00:40:47 -0700

    Create insert.php
    code that inserts post into DB

---
+++ 06SecuringTheApplication/insert.php
@@ -0,0 +1,56 @@
+<?php
+$name = $_POST['name'];
+$topic = $_POST['topic'];
+$comment = $_POST['comment'];
+$dateposted = date('Y-m-d H:i:s');
+$approved = 0;
+
+require_once('variable.php');
+
+//building database connection      	
+$dbc = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die('Connection to the Database failed.');          
+	
+//build query
+$query = "INSERT INTO blog (name, topic, comment, date, approved)"."VALUES('$name','$topic','$comment','$dateposted', '$approved')";
+	
+//talk to database
+$result = mysqli_query($dbc, $query) or die("Query: ". $query."<p>Error: " .mysqli_error($dbc));
+				
+//close connection
+mysqli_close($dbc);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="Profile3/profile3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <title>Securing The Application</title>
+</head>
+
+<body>
+<?php include_once('header.php');?>
+<div id="content" class="container-fluid">
+     <div class="jumbotron jumbotron-fluid text-center">
+        <h1>Your post has been sent for Admin Approval.</h1>
+     </div>
+
+</div>
+
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
+
+
+</html>
