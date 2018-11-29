e95b4cb0fc31611c4679912d54fa6654c5ee691e
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-11-29 00:36:34 -0700

    Create authorize.php
    code for authorizing a user before allowing them to access the approval page.

---
+++ 06SecuringTheApplication/authorize.php
@@ -0,0 +1,9 @@
+<?php
+$username = 'admin';
+$password = 'time';
+if(!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER'] != $username || $_SERVER['PHP_AUTH_PW'] != $password) {
+  header('HTTP/1.1 401 Unauthorized');
+  header('WWW-Authenticate: Basic realm="Up2date "');
+  exit('Access Denied: Username or Password is incorrect.');
+}
+?>
