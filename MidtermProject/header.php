f286b9fbc37eecb9ea473498fa3dd6f17635d1b7
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-11-30 01:30:39 -0700

    Create header.php

---
+++ MidtermProject/header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="http://ammonp.sgedu.site">AP</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Employees</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="register.php">Register</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="manage.php">Manage Employees</a>
+    </li>
+  </ul>
+</nav>
+<br>
