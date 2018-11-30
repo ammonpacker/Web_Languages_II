b532b8642aa2cfc74da4b225b4752fbea1ae6693
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-11-30 01:32:49 -0700

    Create msgSent.php

---
+++ MidtermProject/msgSent.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="Profile3/profile3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <title>Midterm Project</title>
+</head>
+
+<body>
+<?php include_once('header.php');?>
+<div id="content" class="container-fluid">
+     <div class="jumbotron jumbotron-fluid text-center">
+        <h1>Your message has been Sent!</h1>
+     </div>
+</div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
+
+
