02dde11e7cc61c81b9d0e09a9290beffc602b893
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-12-03 06:14:50 -0700

    Create header.php

---
+++ 08JoiningTables/header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="http://ammonp.sgedu.site">AP</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="new.php">Sign up</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="search.php">Search</a>
+    </li>
+
+  </ul>
+  
+</nav>
+
