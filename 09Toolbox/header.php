40d4c523fd8ebfb41d39788563d6c18f3bab5899
Ammon Packer <42591178+ammonpacker@users.noreply.github.com>
2018-12-10 09:39:44 -0700

    Create header.php

---
+++ 09Toolbox/header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="http://ammonp.sgedu.site">AP</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="index.php"><i class="far fa-newspaper"></i> Blog Posts</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="comment.php"><i class="fas fa-pencil-alt"></i> Write a Post</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="search.php"><i class="fas fa-search"></i> Search Posts</a>
+    </li>
+  </ul>
+</nav>
+<br>
